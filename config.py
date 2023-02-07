5213e13455f6775a6718bf9a4fe9b9635eecc48d
santhoshmural <120946243+santhoshmural@users.noreply.github.com>
2023-02-07 12:35:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID = os.environ.get("API_ID", "11319890")
 
 API_HASH = os.environ.get("API_HASH", "4965e574411647c42ffbd8b46040f0a5")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5966564904:AAG9sbcErdLRUMynBxdGAJLXAjzwPLuKc1I") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5670197186:AAHGpfSiCrK7kXCo3M03vyg7FuTn1tX9UDI") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Tamildubbedpublic") 
 
